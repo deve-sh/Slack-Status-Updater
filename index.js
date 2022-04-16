47cbc4bd21f3fa1e3714e2afbe7bc7603bc8f8f9
Devesh Kumar <devesh2027@gmail.com>
2022-04-16 12:35:38 +0530

    feat: Added dotenv for environment variable config

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
 import express from "express";
+import { config } from "dotenv";
+
+config();
 
 const app = express();
+const PORT = process.env.PORT || 8080;
 
 app.use("/api", APIRouter);
+
+app.listen(PORT, () => console.log("Listening at port: ", PORT));
