3099c0de79bda06c638d0bef43d77dbbfdc137a2
Devesh Kumar <devesh2027@gmail.com>
2022-04-21 20:45:24 +0530

    fix: Issues with Starting Slack OAuth Flow

--- controllers/oauth/startSlackOAuth.js
+++ controllers/oauth/startSlackOAuth.js
@@ -1,14 +1,15 @@
 const { InstallProvider } = require("@slack/oauth");
 const { SERVER_ERROR } = require("../../constants");
 
-const startSlackOAuth = async (req, res) => {
+const startSlackOAuth = async (_, res) => {
 	const message = (status, message) => res.status(status).json({ message });
 	const slackStartURL = process.env.SLACK_OAUTH_START_LINK;
 	const clientId = process.env.SLACK_CLIENT_ID;
 	const clientSecret = process.env.SLACK_CLIENT_SECRET;
 	const stateSecret = process.env.SLACK_STATE_SECRET;
+	const redirectUri = process.env.SLACK_REDIRECT_URL;
 
-	if (!slackStartURL || !clientId || !stateSecret)
+	if (!slackStartURL || !clientId || !stateSecret || !redirectUri)
 		return message(500, SERVER_ERROR);
 
 	const installer = new InstallProvider({
@@ -18,7 +19,9 @@ const startSlackOAuth = async (req, res) => {
 	});
 
 	const installURL = await installer.generateInstallUrl({
-		scopes: ["identify", "users.profile:read", "users.profile:write"],
+		scopes: ["commands"],
+		userScopes: ["users.profile:read", "users.profile:write", "identify"],
+		redirectUri,
 	});
 
 	res.redirect(installURL);
