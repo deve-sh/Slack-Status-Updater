a895e5b011b5d87cee43cf316a7510c3c1156ba7
Devesh Kumar <devesh2027@gmail.com>
2022-05-01 15:44:38 +0530

    feat: Added placeholder interactivity route controller for modals and forms

--- routers/hooks.js
+++ routers/hooks.js
@@ -3,11 +3,13 @@ const { Router } = require("express");
 const updateStatus = require("../controllers/hooks/updateStatus");
 const togglePresence = require("../controllers/hooks/togglePresence");
 const appHomeOpened = require("../controllers/hooks/appHomeOpened");
+const interactivity = require("../controllers/hooks/interactivity");
 
 const HooksRouter = Router();
 
 HooksRouter.post("/updatestatus", updateStatus);
 HooksRouter.post("/togglepresence", togglePresence);
 HooksRouter.post("/apphomeopened", appHomeOpened);
+HooksRouter.post("/interactivity", interactivity);
 
 module.exports = HooksRouter;
