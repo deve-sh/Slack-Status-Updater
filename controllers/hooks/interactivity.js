ad61284a7279c23b2824d9bf60e17a9222add493
Devesh Kumar <devesh2027@gmail.com>
2022-05-01 16:40:52 +0530

    feat: Completed interactivity controller for button click for status update

--- controllers/hooks/interactivity.js
+++ controllers/hooks/interactivity.js
@@ -1,3 +1,36 @@
-module.exports = (req, res) => {
-	res.sendStatus(200);
+const axios = require("axios");
+const qs = require("qs");
+const getBotToken = require("../../utils/getBotToken");
+const getStatusUpdateModalView = require("../../utils/getStatusUpdateModalView");
+
+module.exports = async (req, res) => {
+	try {
+		res.sendStatus(200);
+		const payload = JSON.parse(req.body.payload || "{}");
+		const { trigger_id, team = {}, user, actions = [] } = payload;
+		if (
+			team &&
+			user &&
+			actions &&
+			team.id &&
+			user.id &&
+			actions.length &&
+			actions[0].action_id === "open_status_updater_form"
+		) {
+			const [botToken] = await getBotToken(team.id);
+			if (!botToken) return;
+
+			const statusUpdateModalView = getStatusUpdateModalView();
+
+			const args = {
+				token: botToken,
+				trigger_id: trigger_id,
+				view: JSON.stringify(statusUpdateModalView),
+			};
+
+			await axios.post("https://slack.com/api/views.open", qs.stringify(args));
+		}
+	} catch (err) {
+		console.log(err);
+	}
 };
