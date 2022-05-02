241a264cf743a3854d96e1b4f41d7995f369a067
Devesh Kumar <devesh2027@gmail.com>
2022-05-02 14:17:43 +0530

    fix: Small issue with base expiry time computation

--- utils/getBaseExpiryISOTime.js
+++ utils/getBaseExpiryISOTime.js
@@ -1,7 +1,7 @@
 const getBaseExpiryISOTime = () => {
 	const now = new Date();
 	const nextHour = (now.getHours() + 1) % 24;
-	`${nextHour < 10 ? "0" : ""}${nextHour}:00`;
+	return `${nextHour < 10 ? "0" : ""}${nextHour}:00`;
 };
 
 module.exports = getBaseExpiryISOTime;
