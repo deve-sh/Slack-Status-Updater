cb183fdf71494ec141f8addfc1ab64ca88b9b677
Devesh Kumar <devesh2027@gmail.com>
2022-05-02 13:53:47 +0530

    chore: Cleaned up exit condition for interactivity hook

--- controllers/hooks/interactivity.js
+++ controllers/hooks/interactivity.js
@@ -8,15 +8,10 @@ module.exports = async (req, res) => {
 		res.sendStatus(200);
 		const payload = JSON.parse(req.body.payload || "{}");
 		const { trigger_id, team = {}, user, actions = [] } = payload;
-		if (
-			team &&
-			user &&
-			actions &&
-			team.id &&
-			user.id &&
-			actions.length &&
-			actions[0].action_id === "open_status_updater_form"
-		) {
+		if (!(team && user && actions && team.id && user.id && actions.length))
+			return;
+
+		if (actions[0].action_id === "open_status_updater_form") {
 			const [botToken] = await getBotToken(team.id);
 			if (!botToken) return;
 
